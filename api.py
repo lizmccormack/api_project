3a3b36902b1c863853662ef4e44e378cd8c7a1cd
Liz McCormack <ecmccormack5@gmail.com>
2019-08-22 21:34:07 +0000

    create user path framework

--- api.py
+++ api.py
@@ -1,3 +1,7 @@
+
+from flask import (Flask, jsonify, request)
+from flask_pymongo import PyMongo 
+from pymongo import MongoClient
 import os
 from synapsepy import Client
 
@@ -10,25 +14,45 @@ client = Client(
     logging= False # (optional) logs to stdout if True
 )
 
-body = {
-  "logins": [
-    {
-      "email": "test@synapsefi.com"
-    }
-  ],
-  "phone_numbers": [
-    "901.111.1111",
-    "test@synapsefi.com"
-  ],
-  "legal_names": [
-    "Test User 2"
-  ],
-  "extra": {
-    "supp_id": "122eddfgbeafrfvbbb",
-    "cip_tag":1,
-    "is_business": false
-  }
-}
+app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'synapse_db'
+app.config["MONGO_URL"] = 'mongodb://localhost:27017/synapse_db'
+mongo = PyMongo(app)
 
-new_user = client.create_user(body)
+
+@app.route('/')
+def hello(): 
+    """TEST"""
+    return 'welcome to my api'
+
+# Users 
+@app.route('/users', methods=['GET'])
+def get_all_users(): 
+    """View all users."""
+    allusers = client.get_all_users(show_refresh_tokens=True)
+
+    return jsonify(allusers)
+
+@app.route('/users', methods=['POST'])
+def create_user():
+    """Create a new user."""
+    pass
+
+@app.route('/users/<user_id>', methods=['GET'])
+def get_one_user():
+    """View one user."""
+    pass
+
+@app.route('/users/<user_id>' methods=['PUT'])
+def update_user(): 
+    """Update one user."""
+    pass 
+
+@app.route('/users/<user_id>', methods=['DELETE'])
+def delete_user(): 
+    """Delete a user."""
+    pass 
+
+# functionality 
+
 
